858e809067e4c59e022df23f531d637de7b1f7ab
makarroche <macarena@mynft.com>
2023-12-20 17:06:13 -0300

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -9,7 +9,7 @@ module.exports = defineConfig({
   },
 
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents(on: any, config: any) {
       // implement node event listeners here
     },
   },
